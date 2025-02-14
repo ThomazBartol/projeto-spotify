2dd98229fd4ab21b0f69b0ddfaaacd33ea4d14bf
ThomazBartol <thomaz.bartol2006@gmail.com>
2025-02-14 10:36:05 -0300

    Desenvolvendo página de Artistas e Músicas

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,25 +3,27 @@ import ItemList from './ItemList';
 import { artistArray } from '../assets/database/artists';
 import { songsArray } from '../assets/database/songs';
 
-const Main = () => {
+const Main = ({ type }) => {
   return (
     <div className='main'>
         {/* ItemList de Artistas*/}
+        {type === 'artists' | type === undefined ?
         <ItemList 
             title="Artistas" 
-            items={5} 
+            items={10} 
             itemsArray={artistArray} 
             path='/artists'
             idPath='/artist'
-            />
+            /> : (<></>)}
         {/* ItemList de Músicas*/}
+        {type === 'songs' | type === undefined ?
         <ItemList 
             title="Músicas" 
-            items={10} 
+            items={20} 
             itemsArray={songsArray} 
             path='/songs'
             idPath='/song'
-            />
+            /> : (<></>)}
     </div>
   )
 }
--- src/pages/Artist.jsx
+++ src/pages/Artist.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 const Artist = () => {
   return (
-    <div>Artist</div>
+    <div className='artist'>
+      <div className="artist__header"></div>
+
+      <div className="artist__body"></div>
+
+      <Link to='/song/1'>
+        <FontAwesomeIcon className='single-item__icon single-item__icon--artist' icon={faCirclePlay} />
+      </Link>
+    </div>
   )
 }
 
