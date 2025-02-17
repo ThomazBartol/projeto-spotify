23817a7b12ac3364af8d2836620955d20c899bf2
ThomazBartol <thomaz.bartol2006@gmail.com>
2025-02-17 09:20:43 -0300

    Fazendo Response do banco de Artists e Songs

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,7 +1,22 @@
 import express from 'express';
+import { artistArray } from '../../front-end/src/assets/database/artists.js';
+import { songsArray } from '../../front-end/src/assets/database/songs.js';
+
 
 const app = express();
 const PORT = 3001;
+
+app.get('/', (request, response) => {
+    response.send('Endpoints: /artists e /songs');
+})
+
+app.get('/songs', (request, response) => {
+    response.send(songsArray);
+})
+
+app.get('/artists', (request, response) => {
+    response.send(artistArray);
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
