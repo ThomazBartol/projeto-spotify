3d4fe98b58dfbda44ddf12965c381b82a47f70d8
ThomazBartol <thomaz.bartol2006@gmail.com>
2025-02-21 16:07:28 -0300

    Ajustes para o Deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,24 +1,33 @@
 import express from 'express';
 import cors from 'cors';
 import { db } from './connect.js';
+import path from 'path'
+
+const __dirname = path.resolve();
 
 const app = express();
 const PORT = 3001;
 
 app.use(cors());
 
-app.get('/', (request, response) => {
+app.get('/api/', (request, response) => {
     response.send('Endpoints: /artists e /songs');
-})
+});
 
-app.get('/songs', async(request, response) => {
+app.get('/api/songs', async(request, response) => {
     response.send(await db.collection('songs').find({}).toArray());
-})
+});
 
-app.get('/artists', async(request, response) => {
+app.get('/api/artists', async(request, response) => {
     response.send(await db.collection('artists').find({}).toArray());
-})
+});
+
+app.use(express.static(path.join(__dirname, '../front-end/dist')));
+
+app.get('*', async(request, response) => {
+    response.sendFile(path.join(__dirname, '../front-end/dist'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+});
